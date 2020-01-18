47675723dc1799ffe9c79baaab95f2b27ece0f15
Alain Nguyen <onealainn@gmail.com>
2020-01-18 13:14:37 -0500

    Fixed user hashing

--- user/UserController.js
+++ user/UserController.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
+const Crypto = require('crypto')
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended:true}));
@@ -9,6 +10,11 @@ var User = require('./User')
 router.post('/', function(req, res){
   console.log('Requesting post');
   console.log(req.body)
+
+  let salt = Crypto.randomBytes(16).toString('base64')
+  let hash = Crypto.createHmac('sha512',salt).update(req.body.password).digest("base64");
+  let password = salt + "$" + hash;
+  req.body.password = password;
 
   User.create({
     username  : req.body.username,
